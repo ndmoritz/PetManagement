d4a326991485f6292c5db7597d1125c0cc370775
Moritz Maienschein <moritz.maienschein@gmx.de>
2026-09-22 11:27:54 +0200

    [review] add angular packages and set globals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+    },    
+    {
+        path: 'login',
+        component: Login
+    },
+    
+];
